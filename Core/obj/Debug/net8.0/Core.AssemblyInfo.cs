3463bf4bbcc364d022aa60aa2f6720086b264eb2
developer.gomaa@gmail.com <developer.gomaa@gmail.com>
2024-11-29 20:50:20 +0200

    commit chnages

--- Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a356378c90544e4f8a788142047d6a7311ecec9e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46d971b5485d0d4ecfe5830901d623be116db2bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
